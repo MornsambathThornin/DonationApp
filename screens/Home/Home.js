2b01ceb4dc441739e243b01994df45649e33fd0e
Mornsambath Thornin <sambaththornin@gmail.com>
2025-02-19 10:34:53 +0700

    add Button component with styles and integrate into Home screen

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -5,13 +5,19 @@ import Header from '../../components/Header/Header';
 
 // import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
+import Button from '../../components/Button/Button';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={'Thornin Morn.'} types={1} />
-      <Header title={'Thornin Morn.'} types={2} />
-      <Header title={'Thornin Morn.'} types={3} />
+      <Button
+        title={'Donate'}
+        onPress={() => {
+          console.log('Donate button pressed');
+        }}
+      />
+      <Button title={'Donate'} isDisabled={true} />
     </SafeAreaView>
   );
 };
