6b30f3781b3ebb7cb05daf5115ee8ef408590183
Mornsambath Thornin <sambaththornin@gmail.com>
2025-02-26 13:15:55 +0700

    add dotenv support; configure environment variables for Firebase setup; update .gitignore and package files

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,23 @@
 import {initializeApp} from 'firebase/app';
 import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from '@env';
 
 // Extracted values from google-services.json
 const firebaseConfig = {
-  apiKey: 'AIzaSyD2_LiYU8SxDhedPSV4xXCiWEdHrxGyjZ8', // Extracted from google-services.json
-  authDomain: 'donation-application-9066d.firebaseapp.com', // Constructed from project_id
-  projectId: 'donation-application-9066d', // Extracted from google-services.json
-  storageBucket: 'donation-application-9066d.appspot.com', // Extracted from google-services.json
-  messagingSenderId: '420919772689', // Extracted from google-services.json
-  appId: '1:420919772689:android:820fc8e6165442041713b0', // Extracted from google-services.json
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
